657ce9510d43e0e794104e0cc8649e7025e2aad5
E-Drop <erosendo.developer@gmail.com>
2020-05-28 20:23:10 +0200

    Add styles to the app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar/>
+        <div className="search-box">
+          <SearchBar/>
+        </div>
         <MapComponent/>
       </div>
     );
